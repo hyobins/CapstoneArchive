b5105ab68098939e8d321ad5b6003b810b353124
hyobins <dawnrice6299@gmail.com>
2020-09-17 01:32:06 +0900

    naver api 이용 크롤링

--- blog.py
+++ blog.py
@@ -1,11 +1,29 @@
-from bs4 import BeautifulSoup as bs
-from pprint import pprint
 import requests
+import json
+from urllib.parse import quote
+import pandas as pd
+ 
+def call(keyword, start):
+    encText = quote(keyword)
+    url = "https://openapi.naver.com/v1/search/blog?query="+encText+"차박"+"&sort=sim&display=100&start="+str(start)
+    result = requests.get(url=url,
+                          headers={"X-Naver-Client-Id":"doyEYm6zc4qz9ayfO5RA","X-Naver-Client-Secret":"8InHQwMIoT"})
+    print(result)  # Response [200]
+    return result.json()
+ 
+def results(keyword):
+    list = []
+    for num in range(0,10):
+        list = list + call(keyword, num * 110 + 1)['items'] 
+    return list
 
-html = requests.get('https://blog.naver.com/atlas100/222086300913')
-soup = bs(html.text, 'html.parser')
 
-data = soup.find('div', {'class':'se-main-container'})
-text = data.find('span', {'class':'se-fs-fs16 se-ff-nanummyeongjo'})
+listt = []
+location = input('검색하고싶은 지역?')
+result = results(location)
+listt = listt+result
+file = open('result.json','w+')
+file.write(json.dumps(listt))
 
-print(text)
+df = pd.read_json('result.json')
+df.to_csv('/Users/ihyobin/Desktop/crolling_results/crawling_result.csv',encoding='utf-8-sig',index=False)
